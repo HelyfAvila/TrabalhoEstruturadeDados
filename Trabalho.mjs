11f6ef2052ada476cd45e2b4d627870648226457
Helyf Avila <avilahelyf@gmail.com>
2023-09-16 15:05:45 -0300

    alterando

--- Trabalho.mjs
+++ Trabalho.mjs
@@ -1,29 +1,33 @@
 const alunos = [];
 
-function processar(){
+function processar() {
     let opcao = document.getElementById('opcao');
     let opc = Number(opcao.value);
+    let res = document.getElementById('res');
+    
     if(opcao.value.length == 0){
         alert(`[ERRO] Digite um valor válido por favor!`) 
         opcao.value = ""
         return; 
     }else if(opc == 1){
        alert('Cadastramento de alunos');
-        while( alunos.length <= 2){
+        while( alunos.length < 2){
             let nome = prompt('Cadastre um novo aluno. Informe o nome:');
-                    if(nome === null){
+                    if(nome.length == 0){
+                        alert('É necessário informar o nome do aluno!')
                         break;
                     }
             let ra = prompt('Entre com o número do RA');
             if(ra.length == 0 || ra.length > 13 ){
                 alert('O valor não pode ser vazio ou maior do que 13 digitos!')
-                return;
+                break;
             }
             let idade = prompt('Informe a idade do aluno: ');
             if(idade.length == 0){
                 alert('É necessário digitar a idade!')
+                break;
             }
-            let sexo = prompt("Informe o sexo do aluno ( M OU F):");
+            let sexo = prompt("Informe o sexo do aluno ( M OU F ):");
             let media = Number(prompt("Informe a média do aluno: "));
             let resultado = "";
             if(!isNaN(media)){
@@ -43,13 +47,19 @@ function processar(){
               };
             
             alunos.push(aluno);
-            console.log(alunos);
-            
+          
+        }
+   
+        res.innerHTML = "";
+        for (let i = 0; i < alunos.length; i++) {
+        res.innerHTML += `Aluno ${i + 1}:<br>
+                        Nome: ${alunos[i].nome}<br>
+                        RA: ${alunos[i].ra}<br>
+                        Idade: ${alunos[i].idade}<br>
+                        Sexo: ${alunos[i].sexo}<br>
+                        Média: ${alunos[i].media}<br>
+                        Resultado: ${alunos[i].resultado}<br><br>`;
         }
     }
-            
-    
 }
-
-
 
