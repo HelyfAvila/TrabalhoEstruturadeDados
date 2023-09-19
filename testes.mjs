3fce7d5734479c823d13abe19dcdf7583d1472f9
Helyf Avila <avilahelyf@gmail.com>
2023-09-19 20:13:59 -0300

    alterando

--- testes.mjs
+++ testes.mjs
@@ -1,4 +1,11 @@
-const alunos = [];
+const alunos = [{
+    nome:'João',
+    ra:123,
+    idade:12,
+    sexo:'M',
+    media: 10,
+    resultado:'APROVADO'
+}];
 function cadastrarAluno() {
     let res = document.getElementById('res');
     
@@ -43,7 +50,7 @@ function cadastrarAluno() {
         }
         res.innerHTML = "";
         for (let i = 0; i < alunos.length; i++) {
-        res.innerHTML += `Aluno ${i + 1}:<br>
+        res.innerHTML += `
                         Nome: ${alunos[i].nome}<br>
                         RA: ${alunos[i].ra}<br>
                         Idade: ${alunos[i].idade}<br>
@@ -53,9 +60,102 @@ function cadastrarAluno() {
         }
     
 }
-    function nomesCrescente() {
-        alert("kdskdjskj")
+function nomesCrescente(alunos) {
+    const n = alunos.length;
+  
+    for (let i = 0; i < n - 1; i++) {
+  
+      for (let j = 0; j < n - i - 1; j++) {
+        const nomeAtual = alunos[j].nome.toLowerCase();
+        const proximoNome = alunos[j + 1].nome.toLowerCase();
+  
+        if (nomeAtual > proximoNome) {
+          const temp = alunos[j];
+          alunos[j] = alunos[j + 1];
+          alunos[j + 1] = temp;
+        }
+      }
     }
+    gerarRelatorio(alunos);
+  }
+  //lembrar de gerar relatorio GerarRelatorioRA
+  function GerarRelatorioOCN(vetor) {
+    CrescentePorNome(vetor)
+    GerarRelatorio(vetor,false)
+  }
+  function GerarRelatorio(vetor, aprovados) {
+    for (let i = 0; i < vetor.length; i++) {
+      if (!aprovados) {
+        console.log(`nome: ${vetor[i].nome}`)
+        console.log(`ra: ${vetor[i].ra}`)
+        console.log(`idade: ${vetor[i].idade}`)
+        console.log(`sexo: ${vetor[i].sexo}`)
+        console.log(`media: ${vetor[i].media}`)
+        console.log(`resultado: ${vetor[i].resultado}`)
+      } else {
+        if (vetor[i].resultado == "aprovado") {
+          console.log(`nome: ${vetor[i].nome}`)
+          console.log(`ra: ${vetor[i].ra}`)
+          console.log(`idade: ${vetor[i].idade}`)
+          console.log(`sexo: ${vetor[i].sexo}`)
+          console.log(`media: ${vetor[i].media}`)
+          console.log(`resultado: ${vetor[i].resultado}`)
+        }
+      }
+    }
+  }
+  function gerarRelatorio(alunos){
+    let tabela = document.querySelector('table.tabelaRelatorio');
+    tabela.innerHTML = "";
+    primeiraLinha(alunos);
+    for(let i=0; i < alunos.length; i++){
+        let linha = document.createElement('tr');
+        let nomes = document.createElement('td');
+        let ra = document.createElement('td');
+        let idade = document.createElement('td');
+        let sexo = document.createElement('td');
+        let media = document.createElement('td');
+        let resultado = document.createElement('td');
+        tabela.appendChild(linha);
+        linha.appendChild(nomes);
+        linha.appendChild(ra);
+        linha.appendChild(idade);
+        linha.appendChild(sexo);
+        linha.appendChild(media);
+        linha.appendChild(resultado);
+        nomes.textContent=`${alunos[i].nome}`;
+        ra.textContent=`${alunos[i].ra}`;
+        idade.textContent=`${alunos[i].idade}`;
+        sexo.textContent=`${alunos[i].sexo}`;
+        media.textContent=`${alunos[i].media}`;
+        resultado.textContent=`${alunos[i].resultado}`;
+    }
+  }
+  function primeiraLinha(alunos){
+    let tabela = document.querySelector('table.tabelaRelatorio');
+        let linha = document.createElement('tr');
+        let nomes = document.createElement('td');
+        let ra = document.createElement('td');
+        let idade = document.createElement('td');
+        let sexo = document.createElement('td');
+        let media = document.createElement('td');
+        let resultado = document.createElement('td');
+        tabela.appendChild(linha);
+        linha.appendChild(nomes);
+        linha.appendChild(ra);
+        linha.appendChild(idade);
+        linha.appendChild(sexo);
+        linha.appendChild(media);
+        linha.appendChild(resultado); 
+        nomes.textContent = `NOME DO ALUNO:`;
+        ra.textContent = `RA DO ALUNO:`;
+        idade.textContent = `IDADE:`;
+        sexo.textContent = `SEXO:`;
+        media.textContent = `MÉDIA:`;
+        resultado.textContent = `RESULTADO:`
+
+  }
+
 
     function escolherOpcao(){
         let opcao = document.getElementById('opcao');
@@ -71,7 +171,7 @@ function cadastrarAluno() {
             cadastrarAluno();
         }else if(opc === 2){
             res.innerHTML = ""
-            nomesCrescente();
+            nomesCrescente(alunos);
         }else if(opc === 3){
             res.innerHTML = ""
             
